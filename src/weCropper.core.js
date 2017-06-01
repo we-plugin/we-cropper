c764c1c85be5139cf6b4d4aeffb40dd6640fd14f
sail <505441575@qq.com>
2017-06-01 23:57:19 +0800

    fixed crop image quality && update api -> getCropperImage

--- src/weCropper.core.js
+++ src/weCropper.core.js
@@ -1,4 +1,4 @@
-const __version__ = '1.1.2'
+const __version__ = '1.1.3'
 
 const DEFAULT = {
 	id: {
@@ -354,6 +354,7 @@ export default class weCropper {
 	_methods () {
 		const self = this
 		const { windowWidth } = self.getDevice()
+		const deviceRadio = windowWidth / 750
 
 		self.updateCanvas = () => {
 			if (self.croperTarget) {
@@ -379,9 +380,9 @@ export default class weCropper {
 
 					self.croperTarget = res.path
 					self.rectX = 0
-					self.baseWidth= width * windowWidth / 750
-					self.baseHeight = width * windowWidth / (innerAspectRadio * 750)
-					self.rectY = (height * windowWidth / 750 - self.baseHeight) / 2
+					self.baseWidth = width * deviceRadio
+					self.baseHeight = width * deviceRadio / innerAspectRadio
+					self.rectY = (height * deviceRadio - self.baseHeight) / 2
 
 					self.imgLeft = self.rectX
 					self.imgTop = self.rectY
@@ -399,15 +400,41 @@ export default class weCropper {
 			return self
 		}
 
-		self.getCropperImage = (callback) => {
+		self.getCropperImage = (...args) => {
 			const { id } = self
+			const ARG_TYPE = toString.call(args[0])
 
-			wx.canvasToTempFilePath({
-				canvasId: id,
-				success (res) {
-					typeof callback === 'function' && callback(res.tempFilePath)
-				}
-			})
+			switch (ARG_TYPE) {
+				case '[object Object]':
+					let { x = 0, y = 0, width = self.width * deviceRadio, height = self.height * deviceRadio, quality = 10 } = args[0]
+
+					if (typeof(quality) !== 'number') {
+						console.error(`quality：${quality} is invalid`)
+					} else if (quality < 0 || quality > 10) {
+						console.error(`quality should be ranged in 0 ~ 10`)
+					}
+					wx.canvasToTempFilePath({
+						canvasId: id,
+						x,
+						y,
+						width,
+						height,
+						destWidth: width * quality / (deviceRadio * 10),
+						destHeight: height * quality / (deviceRadio * 10),
+						success (res) {
+							typeof args[args.length - 1] === 'function' && args[args.length - 1](res.tempFilePath)
+						}
+					}); break
+				case '[object Function]':
+					wx.canvasToTempFilePath({
+						canvasId: id,
+						destWidth: self.width,
+						destHeight: self.height,
+						success (res) {
+							typeof args[args.length - 1] === 'function' && args[args.length - 1](res.tempFilePath)
+						}
+					}); break
+			}
 
 			return self
 		}
